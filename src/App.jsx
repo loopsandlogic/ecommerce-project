f8e266dd980d3cdbfc1707666736d041b848f573
loopsandlogic <loopsandlogic@gmail.com>
2026-01-01 15:48:16 -0600

    Create multiple pages using react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
 import './App.css'
 import { StrictMode } from 'react'
 
 function App() {
   return (
-      <HomePage />
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<div>Test Checkout Page</div>} />
+    </Routes>  
   )
-  
 }
 
 export default App
